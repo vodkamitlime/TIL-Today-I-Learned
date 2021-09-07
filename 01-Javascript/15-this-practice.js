6b3231537f93730331db723af366332c7212ef8b
Haeun Kang <tejavascript@gmail.com>
2021-09-07 22:03:56 +0900

    2021 Sep 07: This-practice

---
+++ 01-Javascript/15-this-practice.js
@@ -0,0 +1,10 @@
+var test1 = 'hello'
+let test2 = 'hello world'
+const test3 = 'hello world bye'
+test4 = 'hello world bye world'
+console.log(this)
+console.log(this.test1)
+console.log(this.test2)
+console.log(this.test3)
+console.log(this.test4)
+
