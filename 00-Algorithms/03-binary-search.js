0885f70d452efc4fdfc271b8f2243bc57b9e1c13
Haeun Kang <tejavascript@gmail.com>
2021-10-19 14:00:00 +0900

    2021 Oct 20: Binary search algorithm

--- 00-Algorithms/03-binary-search.js
+++ 00-Algorithms/03-binary-search.js
@@ -1,3 +1,10 @@
+// 이진탐색 : 정렬되어 있는 리스트에서 탐색 범위를 절반씩 좁혀가며 데이터를 탐색하는 방법
+// 이진 탐색은 매우 빠른 속도로 데이터를 찾을 수 있지만 반드시 데이터가 정렬되어있어야만 한다
+// 시간복잡도:O(logN)
+
+
+출처: https://im-developer.tistory.com/126 [Code Playground]
+
 function binarySearch (target, dataArray) {
     let low = 0;
     let high = dataArray.length - 1;
