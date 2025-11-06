f2891517af424b1ddc15cb44a7202f6dfb1a845e
vodkamitlime <tejavascript@gmail.com>
2025-11-06 23:47:43 +0900

    2025-11-06-Problem-bitwise-operator

---
+++ 01-Language-Frameworks/05-C/990.Bitwise-operator.c
@@ -0,0 +1,8 @@
+// First 2 bits = type (00 human, 01 oak, 10 undead, 11 elf)
+// Next 6 bits = level (0~63)
+// Next 12 bits = power (0~4095)
+// Next 12 bits = Mana (0~4095)
+// When character information is given as unsigned int for input, print the type/level/power/mana of the character
+int main() {
+    return 0;
+}
